eac35bd2729f3a061f183dad6b595b68ae1ac11e
jdevalk <joost@yoast.com>
2020-02-13 12:12:52 +0100

    Force gutenberg

--- src/default-filters.php
+++ src/default-filters.php
@@ -2,4 +2,13 @@
 
 defined( 'ABSPATH' ) or exit;
 
+add_filter( 'use_block_editor_for_post', 'wpkb_gutenberg', 10, 2 );
+
+function wpkb_gutenberg( $use_block_editor, $post ) {
+    if ( $post->post_type === 'wpkb-article' ) {
+        return true;
+    }
+    return $use_block_editor;
+}
+
 require __DIR__ . '/table-of-contents/filters.php';
